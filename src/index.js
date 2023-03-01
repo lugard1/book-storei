48b8c2afcf9ebdd783c25bca08840beb09edc5bd
Lugard Agu <agulugard@gmail.com>
2023-03-01 11:47:56 +0100

    removed eslints inline disable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter } from 'react-router-dom';
-
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
