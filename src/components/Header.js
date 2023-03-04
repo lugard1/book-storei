b98025bbc6bc175f41d3a858d72f93fb753101f9
Lugard Agu <agulugard@gmail.com>
2023-03-04 11:16:15 +0100

    update headers.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
+import classNames from './Header.module.css';
 
 const Header = () => (
-  <header>
-    <nav>
+  <header className={classNames.header}>
+    <nav className={classNames.navigator}>
       <div>
-        <h1>Bookstore CMS</h1>
+        <h1 className={classNames.header_text}>Bookstore CMS</h1>
       </div>
-      <ul>
-        <li>
+      <ul className={classNames.nav_Links}>
+        <li className={classNames.books}>
           <NavLink to="/">BOOK</NavLink>
         </li>
-        <li>
+        <li className={classNames.categories}>
           <NavLink to="/categories">CATEGORIES</NavLink>
         </li>
       </ul>
-      <div>
-        <button type="button">
+      <div className={classNames.oval}>
+        <button type="button" className={classNames.avatarBtn}>
           <FaUser />
         </button>
       </div>
